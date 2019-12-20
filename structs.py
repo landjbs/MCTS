c20454d2dd3b4ff5d0f22afb0a6e408ad7cf2e39
Landon Smith <landjbs@gmail.com>
2019-12-20 14:35:03 -0500

    Board header init

--- structs.py
+++ structs.py
@@ -8,22 +8,20 @@ class Player(object):
 
 class Board(object):
     def __init__(self, size, coverPercent):
-        ''' Builds board object COVER PERCENT IS BEFORE REPETITION '''
+        '''
+        Builds board object
+        Args:
+            size:           Length of one axis of square board
+            coverPercent:   APPROX wall cover percent of board. Does not account
+                            for repetition. If cover percent is 1, entire board
+                            will be covered except for edge paths.
+        '''
         self.board = np.zeros((size, size, 3))
         # add walls
         coverNum = floor(coverPercent * (size-1)**2)
         xCover = np.random.randint(1, size-1, coverNum)
         yCover = np.random.randint(1, size-1, coverNum)
-
-
-
-
-        while len(coverLocs) < coverNum:
-            x = np.random.randint(1, (size-1))
-            y = np.random.randint(1, (size-1))
-            if not (x, y) in coverLocs:
-                self.board[x, y, 1] = 1
-                coverLocs.add((x, y))
+        self.board[xCover, yCover, 1] = 1
         # add players
         self.board[[0,size-1], [0, size-1], 2] = 1
 
