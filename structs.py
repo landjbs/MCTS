d3db92ab4fd56b4db51b6cbd146e4f4f742085b5
Landon Smith <landjbs@gmail.com>
2019-12-20 14:20:13 -0500

    cover calcs v1

--- structs.py
+++ structs.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import floor
 import matplotlib.pyplot as plt
 
 class Player(object):
@@ -6,9 +7,13 @@ class Player(object):
         pass
 
 class Board(object):
-    def __init__(self, size):
+    def __init__(self, size, coverPercent):
         self.board = np.zeros((size, size, 3))
-        
+        coverNum = floor(coverPercent * size**2)
+        coverXs, coverYs = set(), set()
+        while len(coverLocs) < coverBlocks:
+            coverXs.add(np.random.randint(0, size))
+            coverYs.add(np.random.randint(0, size))
 
     def visualize(self):
         plt.imshow(self.board)
