fe5ffec6fd3f5700e708772f8ed512f472901483
Landon Smith <landjbs@gmail.com>
2019-12-20 14:51:22 -0500

    cover percent change

--- structs.py
+++ structs.py
@@ -7,7 +7,7 @@ class Player(object):
         pass
 
 class Board(object):
-    def __init__(self, size, coverPercent):
+    def __init__(self, size, p):
         '''
         Builds board object for laser tag. Stored as third order tensor of shape
         (size, size, 3) where last three dims correspond to shots, walls,
@@ -15,23 +15,23 @@ class Board(object):
         higher-level Game object.
         Args:
             size:           Length of one axis of square board
-            coverPercent:   APPROX wall cover percent of board. Does not account
-                            for repetition. If coverPercent is 1, entire board
+            p:              APPROX wall cover percent of board. Does not account
+                            for repetition. If p is 1, entire board
                             will be covered except for edge paths.
         '''
         self.board = np.zeros((size, size, 3))
         # add walls
-        if (coverPercent == 0):
+        if (p == 0):
             pass
-        elif (coverPercent == 1):
+        elif (p == 1):
             self.board[1:size-1, 1:size-1, 1] = 1
-        elif (0 < coverPercent < 1):
-            coverNum = floor(coverPercent * (size-1)**2)
+        elif (0 < p < 1):
+            coverNum = floor(p * (size-1)**2)
             xCover = np.random.randint(1, size-1, coverNum)
             yCover = np.random.randint(1, size-1, coverNum)
             self.board[xCover, yCover, 1] = 1
         else:
-            raise ValueError('coverPercent must be in range [0, 1]')
+            raise ValueError(f'Expected p in range [0, 1], but found {p}.')
         # add players
         self.board[[0,size-1], [0, size-1], 2] = 1
 
@@ -46,10 +46,16 @@ class Board(object):
         0 - up, 1 - right, 2 - down, 3 - left. Go as far as they can before
         hitting a wall.
         '''
-        if isX:
-            self.board[]
+        if (d == 0):
+            'up'
+        elif (d == 1):
+            'right'
+        elif (d == 2):
+            'down'
+        elif (d == 3):
+            'left'
         else:
-            self.board
+            raise ValueError(f'Expected d in range [0, 3], but found {d}.')
 
     def vis(self, show=True, outPath=False):
         plt.imshow(self.board)
