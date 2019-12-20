286fcc4b65a6205bfcc83df10f4187f14af7458c
Landon Smith <landjbs@gmail.com>
2019-12-20 14:27:33 -0500

    visualize imp

--- structs.py
+++ structs.py
@@ -10,11 +10,20 @@ class Board(object):
     def __init__(self, size, coverPercent):
         self.board = np.zeros((size, size, 3))
         coverNum = floor(coverPercent * size**2)
-        coverXs, coverYs = set(), set()
-        while len(coverLocs) < coverBlocks:
-            coverXs.add(np.random.randint(0, size))
-            coverYs.add(np.random.randint(0, size))
+        coverLocs = set([(0,0), (size, size)])
+        while len(coverLocs) < coverNum:
+            x = np.random.randint(0, size)
+            y = np.random.randint(0, size)
+            if not (x, y) in coverLocs:
+                self.board[x, y, 1] = 1
+                coverLocs.add((x, y))
 
-    def visualize(self):
+    def visualize(self, show=True, outPath=False):
         plt.imshow(self.board)
-        plt.show()
+        if outPath:
+            plt.savefig(outPath)
+        if show:
+            plt.show()
+
+x = Board(10, 0.1)
+x.visualize()
