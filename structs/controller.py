486a69bed7ff4dd87c6bbac617e86d2e0fd20e12
Landon Smith <landjbs@gmail.com>
2019-12-22 22:54:43 -0500

    conv init

--- structs/controller.py
+++ structs/controller.py
@@ -1,6 +1,3 @@
-import torch
-
-
 class Controller(object):
     def __init__(self, name):
         self.name = name
---
+++ structs/models.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+
+
+class Conv(nn.Module):
+    def __init_(self):
+        super(Conv, self).__init__()
