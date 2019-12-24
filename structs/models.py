d9413c1a4053b464cfba83aa69a78f8a5ecb4be4
Landon Smith <landjbs@gmail.com>
2019-12-23 20:20:00 -0700

    conv layer prog

--- structs/models.py
+++ structs/models.py
@@ -5,3 +5,14 @@ import torch.nn as nn
 class Conv(nn.Module):
     def __init_(self):
         super(Conv, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.drop_out = nn.Dropout()
+        self.fc1 = nn.Linear(7 * 7 * 64, 1000)
+        self.fc2 = nn.Linear(1000, 10)
---
+++ structs/tree.py
@@ -0,0 +1,7 @@
+class MCTS(object):
+    ''' Runs a single MCTS from current position on board '''
+    def __init__(self, loc, board):
+        self.loc = loc
+        self.board = board
+
+    def 
