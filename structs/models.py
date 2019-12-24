0b3755fd151414e46d9764cf9ce3200a9c223a41
Landon Smith <landjbs@gmail.com>
2019-12-24 10:51:12 -0700

    forward imp

--- structs/models.py
+++ structs/models.py
@@ -13,6 +13,15 @@ class Conv(nn.Module):
             nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
-        self.drop_out = nn.Dropout()
-        self.fc1 = nn.Linear(7 * 7 * 64, 1000)
-        self.fc2 = nn.Linear(1000, 10)
+        self.dropout = nn.Dropout()
+        self.lin1 = nn.Linear(7 * 7 * 64, 1000)
+        self.lin2 = nn.Linear(1000, 10)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.conv2(x)
+        out = out.reshape(out.size(0), -1)
+        out = self.dropout(out)
+        out = self.lin1(out)
+        out = self.lin2(out)
+        return out
