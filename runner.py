17677bcd6914fd350205c991e5c921794c0fc6ef
Landon Smith <landjbs@gmail.com>
2019-12-24 11:28:18 -0700

    board size updates

--- runner.py
+++ runner.py
@@ -1,15 +1,14 @@
 from structs.models import Conv
+from structs.player import Player
+from structs.controller import Human, Dummy, Bot
+from structs.game import Game
 
-# from structs.player import Player
-# from structs.controller import Human, Dummy, Bot
-# from structs.game import Game
-#
-#
-# bSize = 4
-# p1 = Player(1, 1, Bot('p1'))
-# p2 = Player(bSize, bSize, Bot('p2'))
-#
-# for _ in range(1):
-#     x = Game([p1, p2], bSize, 0)
-#     x.board.vis()
-#     x.play(1000)
+
+bSize = 25
+p1 = Player(1, 1, Bot('p1'))
+p2 = Player(bSize, bSize, Bot('p2'))
+
+for _ in range(1):
+    x = Game([p1, p2], bSize, 0.3)
+    x.board.vis()
+    x.play(1000)
--- structs/models.py
+++ structs/models.py
@@ -6,15 +6,17 @@ class Conv(nn.Module):
     '''
     The Conv model is tasked with prediciting a vector (p) of move probabilities
     across the avaliable moves and a scalar (v) of win probability at the
-    current state. To avoid shape issues, p always has length 8. 
+    current state. To avoid shape issues, p always has length 8. Input board
+    is a 4th order tensor with shape (boardSize, boardSize, 4) where the last
+    order has indicies (0-shots, 1-walls, 2-enemies, 3-player).
     '''
-    def __init_(self, lr):
+    def __init_(self, lr, boardSize=25):
         super(Conv, self).__init__()
         # layers
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(4, 32, kernel_size=1, stride=1, padding=0),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=1, stride=1))
+            nn.MaxPool2d(kernel_size=2, stride=2))
         self.conv2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
@@ -25,6 +27,8 @@ class Conv(nn.Module):
         # optimizers and loss
         self.optim = torch.optim.Adam(self.parameters(), lr=lr)
         self.criterion = nn.CrossEntropyLoss()
+
+    # def calc_padding(self):
 
     def forward(self, x):
         out = self.conv1(x)
