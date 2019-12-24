6ca31b9327f2a8de6d8f377112e3c0035736b6ce
Landon Smith <landjbs@gmail.com>
2019-12-24 10:53:00 -0700

    optim and loss

--- structs/models.py
+++ structs/models.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 
 
 class Conv(nn.Module):
-    def __init_(self):
+    def __init_(self, lr):
         super(Conv, self).__init__()
+        # layers
         self.conv1 = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
@@ -16,6 +17,9 @@ class Conv(nn.Module):
         self.dropout = nn.Dropout()
         self.lin1 = nn.Linear(7 * 7 * 64, 1000)
         self.lin2 = nn.Linear(1000, 10)
+        # optimizers and loss
+        self.optim = torch.optim.Adam(self.parameters(), lr=lr)
+        self.criterion = nn.CrossEntropyLoss()
 
     def forward(self, x):
         out = self.conv1(x)
@@ -25,3 +29,5 @@ class Conv(nn.Module):
         out = self.lin1(out)
         out = self.lin2(out)
         return out
+
+    def train_step(self, x):
