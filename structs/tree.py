5898cc3f06016c6373be499da14967fb88bd451b
Landon Smith <landjbs@gmail.com>
2019-12-26 10:23:27 -0700

    load unexplored complete

--- structs/tree.py
+++ structs/tree.py
@@ -2,8 +2,8 @@ from collections import defaultdict
 
 class Node(object):
     ''' Node Monte Carlo Tree Search '''
-    def __init__(self, board, parent=None):
-        self.board = board
+    def __init__(self, player, parent=None):
+        self.player = player
         self.parent = parent
         self.children = []
         self.visitNum = 0
@@ -13,8 +13,13 @@ class Node(object):
     def load_unexplored(self):
         ''' Load unexplored actions from node '''
         if (self.unexplored == None):
-            self.unexplored = self.board.get_moves()
+            self.unexplored = self.player.board.get_moves((self.player.y,
+                                                           self.player.x))
+        return self.unexplored
 
+    def calc_q(self):
+        ''' Calcs q value of current state '''
+        pass
 
 class MCTS(object):
     ''' Runs a single MCTS from current position on board '''
